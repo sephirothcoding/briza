292ec0bebbf0daa8aa7527c3b0a2c79df4f9678b
Sephiroth <78663409+sephirothcoding@users.noreply.github.com>
2023-07-07 09:46:19 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,62 +1,62 @@
 var RENDERER = {
-	init : function(){
+	init: function () {
 		this.setParameters();
 		this.reconstructMethods();
 		this.render();
 	},
-	setParameters : function(){
+	setParameters: function () {
 		this.$container = $('#jsi-butterfly-container');
 		this.width = this.$container.width();
 		this.height = this.$container.height();
-		this.context = $('<canvas />').attr({width : this.width, height : this.height}).appendTo(this.$container).get(0).getContext('2d');
+		this.context = $('<canvas />').attr({ width: this.width, height: this.height }).appendTo(this.$container).get(0).getContext('2d');
 		this.butterflies = [new BUTTERFLY(this)];
 		this.particles = [];
 		this.count = this.getRandomValue(100, 300);
 	},
-	reconstructMethods : function(){
+	reconstructMethods: function () {
 		this.render = this.render.bind(this);
 	},
-	getRandomValue : function(min, max){
+	getRandomValue: function (min, max) {
 		return min + (max - min) * Math.random();
 	},
-	render : function(){
+	render: function () {
 		requestAnimationFrame(this.render);
-		
+
 		var context = this.context;
 		context.save();
 		context.fillStyle = 'hsla(0, 0%, 0%, 0.3)';
 		context.fillRect(0, 0, this.width, this.height);
 		context.globalCompositeOperation = 'lighter';
-		
-		for(var i = this.particles.length - 1; i >= 0; i--){
-			if(!this.particles[i].render(context)){
+
+		for (var i = this.particles.length - 1; i >= 0; i--) {
+			if (!this.particles[i].render(context)) {
 				this.particles.splice(i, 1);
 			}
 		}
 		context.restore();
-		
-		for(var i = this.butterflies.length - 1; i >= 0; i--){
-			if(!this.butterflies[i].render(context)){
+
+		for (var i = this.butterflies.length - 1; i >= 0; i--) {
+			if (!this.butterflies[i].render(context)) {
 				this.butterflies.splice(i, 1);
 			}
 		}
-		if(--this.count < 0){
+		if (--this.count < 0) {
 			this.count = this.getRandomValue(100, 300);
 			this.butterflies.push(new BUTTERFLY(this));
 		}
 	}
 };
-var BUTTERFLY = function(renderer){
+var BUTTERFLY = function (renderer) {
 	this.renderer = renderer;
 	this.init();
 };
 BUTTERFLY.prototype = {
-	DELTA_THETA : Math.PI / 50,
-	DELTA_PHI : Math.PI / 100,
-	THRESHOLD : 100,
-	DELTA_PARTICLE : 2,
-	
-	init : function(){
+	DELTA_THETA: Math.PI / 50,
+	DELTA_PHI: Math.PI / 100,
+	THRESHOLD: 100,
+	DELTA_PARTICLE: 2,
+
+	init: function () {
 		this.theta = 0;
 		this.phi = 0;
 		this.x = this.renderer.getRandomValue(this.THRESHOLD, this.renderer.width - this.THRESHOLD);
@@ -65,31 +65,31 @@ BUTTERFLY.prototype = {
 		this.vy = -2;
 		this.swingRate = this.renderer.getRandomValue(0.5, 1);
 	},
-	createParticles : function(){
-		for(var i = 0; i < this.DELTA_PARTICLE; i++){
+	createParticles: function () {
+		for (var i = 0; i < this.DELTA_PARTICLE; i++) {
 			this.renderer.particles.push(new PARTICLE(this.renderer, this.x, this.y));
 		}
 		return this.y >= -this.THRESHOLD;
 	},
-	render : function(context){
+	render: function (context) {
 		context.save();
 		context.translate(this.x, this.y);
 		context.rotate(Math.atan2(this.vx, -this.vy) / 10);
-		
-		for(var i = -1; i <= 1; i += 2){
+
+		for (var i = -1; i <= 1; i += 2) {
 			context.save();
 			context.scale(i, 1);
-			
+
 			var gradient = context.createRadialGradient(0, 0, 0, 0, 0, 80),
 				rate = Math.sin(this.theta / 4);
 			gradient.addColorStop(0, 'hsl(45, 100%, 50%)');
-			gradient.addColorStop(0.3, 'hsl(291, 46%,  ' + (76 + 10 * rate) +  '%)');
-			gradient.addColorStop(0.5, 'hsl(291, 46%, ' + (76 + 20 * rate) +  '%)');
-			gradient.addColorStop(1, 'hsl(291, 46%, ' + (76 + 30 * rate) +  '%)');
+			gradient.addColorStop(0.3, 'hsl(45, 100%, ' + (76 + 10 * rate) + '%)');
+			gradient.addColorStop(0.5, 'hsl(45, 100%, ' + (76 + 20 * rate) + '%)');
+			gradient.addColorStop(1, 'hsl(45, 100%, ' + (76 + 30 * rate) + '%)');
 			context.lineWidth = 3;
 			context.strokeStyle = 'hsl(45, 100%, 50%)';
 			context.fillStyle = gradient;
-			
+
 			context.save();
 			context.scale(0.8 + 0.2 * Math.cos(this.theta + Math.PI / 10), 1);
 			context.beginPath();
@@ -100,7 +100,7 @@ BUTTERFLY.prototype = {
 			context.fill();
 			context.stroke();
 			context.restore();
-			
+
 			context.save();
 			context.scale(0.8 + 0.2 * Math.cos(this.theta), 1);
 			context.beginPath();
@@ -111,28 +111,9 @@ BUTTERFLY.prototype = {
 			context.fill();
 			context.stroke();
 			context.restore();
-			
+
 			context.lineWidth = 2;
 			context.strokeStyle = 'hsl(45, 100%, 50%)';
 			context.beginPath();
 			context.moveTo(-2, -10);
-			context.bezierCurveTo(-5, -20, -3 - Math.sin(this.theta), -30, -8 - Math.sin(this.theta), -40);
-			context.stroke();
-			context.restore();
-		}
-		context.save();
-		var gradient = context.createLinearGradient(-3, 0, 3, 0);
-		gradient.addColorStop(0, 'hsl(45, 100%, 50%)');
-		gradient.addColorStop(0.5, 'hsl(45, 100%, 50%)');
-		gradient.addColorStop(1, 'hsl(45, 100%, 50%)');
-		context.fillStyle = gradient;
-		context.beginPath();
-		context.moveTo(0, -10);
-		context.arc(0, -10, 3, 0, Math.PI * 2, false);
-		context.fill();
-		
-		context.beginPath();
-		context.moveTo(3, -8);
-		context.arc(0, -8, 3, 0, Math.PI, false);
-		context.stroke();
-		context
+			context.bezierCurveTo(-5, -20, -3 - Math.sin(this.theta), -30, -8 - Math
